771433706ff02f44332d6871b6546d08744318cf
adkantor <kantor.a@usa.net>
2020-09-23 10:26:40 +0200

    Delete proba_crawl.py

--- proba_crawl.py
+++
@@ -1,8 +0,0 @@
-from pagerank import crawl
-
-corp_dirs = ['corpus0','corpus1','corpus2']
-for corp_dir in corp_dirs:
-    corpus = crawl(corp_dir)
-    print(corp_dir)
-    print(corpus)
-    print('\n')
